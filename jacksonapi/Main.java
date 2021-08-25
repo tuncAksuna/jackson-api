5588e889780167fd0a734d7e31914fbfabd8cb56
tuncAksuna <aksuna.tunc@gmail.com>
2021-08-26 00:17:05 +0300

    object to json , json to object

--- jacksonapi/Main.java
+++ jacksonapi/Main.java
@@ -1,6 +1,11 @@
 package com.tuncode.jacksonapi;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +17,7 @@ public class Main {
         try {
 
             ObjectMapper mapper = new ObjectMapper();
+
             Example exampleClass = mapper.readValue(new File("src/myFile.json"), Example.class);
             System.out.println("Age : " + exampleClass.getAge() + "\n" +
                     "First name : " + exampleClass.getFirstName() + "\n" +
@@ -24,5 +30,27 @@ public class Main {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+        // Example : I will create a JSON string (following) with Java object class(created) and deserialize it to java class and the serialize it to an JSON String
+
+        ObjectMapper objMapper = new ObjectMapper(); // provides functionality for reading and writing JSON
+        String jsonString = "{\"name\":\"Cem Tunc\",\"age\":22}";
+
+        try {
+            Student javaClass = objMapper.readValue(jsonString, Student.class); // deserilization JSON to object - readValue() method get the object from the JSON
+            System.out.println("Deserilazationing JSON -> Object: " + javaClass);
+
+            jsonString = objMapper.writerWithDefaultPrettyPrinter().writeValueAsString(javaClass); // serialazation object to JSON
+            System.out.println("Serializationing Object -> JSON: " + jsonString);
+
+        } catch (JsonParseException ex) {
+            ex.printStackTrace();
+        } catch (JsonMappingException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
     }
 }
---
+++ jacksonapi/TreeModel.java
@@ -0,0 +1,11 @@
+package com.tuncode.jacksonapi;
+
+public class TreeModel {
+
+    public static void main(String[] args) {
+        // Tree model to represent the JSON structrue , and perform the CRUD operations via "JsonNode"
+
+        // Tree Model is useful, especially in cases where a JSON structure does not map nicely to Java classes.
+
+    }
+}
